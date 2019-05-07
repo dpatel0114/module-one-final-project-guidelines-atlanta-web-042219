d2363bfb575269e1d611d2de5fd06c778dab33a4
github username <github email address>
2019-05-07 17:48:51 -0400

    changing app controller

---
+++ lib/app_controller.rb
@@ -0,0 +1,17 @@
+class AppController
+
+def new_user
+User.create(name:"#{first_name, last_name}", contact_email:"#{contact_email}")
+end
+def new_user
+User.create(name:"John Michael", contact_email:"jmichael@gmail.com")
+end
+def new_user
+  User.create(name:"Stuart Johnson", contact_email:"sjohnson@gmail.com")
+end
+
+
+
+
+
+end
